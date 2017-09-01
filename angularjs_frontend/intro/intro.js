f1a57ef8383664477317e51680525507ae61be2a
Darren Hoyland <darren.hoyland@canonical.com>
2017-09-01 21:24:39 +0100

    Tidied up a bit

--- angularjs_frontend/intro/intro.js
+++ angularjs_frontend/intro/intro.js
@@ -3,20 +3,11 @@ app.controller('intro_controller', [
     '$scope', 'StatementService', '$location', '$interval', '$q', '$rootScope',
     function($scope, StatementService, $location, $interval, $q, $rootScope) {
 
-        $scope.reload = function () {
-            var response = StatementService.query();
-            //var response = StatementService.get({'section.name': 'About'});
-            if (angular.isUndefined($scope.statements)){
-                $scope.statements = response;
-            } else{
-                $q.all([response.$promise]).then(function(data) {
-                    $scope.statements = data[0];
-                });
-            };
-        };
-        $scope.reload();
-        //$interval($scope.reload, 1000);
         $rootScope.page = $location.path();
+
+        $scope.statements = StatementService.query();
+        //var response = StatementService.get({'section.name': 'About'});
+
     }
 
 ]);
